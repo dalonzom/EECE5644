376ef89dbb46728b10b7e9db43da0f184ff5a1c1
ryanhubel <ryanhub@att.net>
2019-04-18 21:19:16 -0400

    Normalization, initialization, and exponential tuning

--- DCP/vm4.m
+++ DCP/vm4.m
@@ -10,29 +10,29 @@ for j = 1:size(test,1)
     three = 0;
     for i = 1:numAttributes
         if (guess{j}.(names{i}) ~= 0)
-            % Only use if the interval is large enough 
+            % Only use if the interval is large enough
             if (training_set{i}.intervals(guess{j}.(names{i+numAttributes}),2) - training_set{i}.intervals(guess{j}.(names{i+numAttributes}),1) > intervalCap)
-            totalCount = sum(training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),:)); 
-            oneCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),1); 
-            twoCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),2); 
-            threeCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),3); 
-            % If above voting cap, set to voting cap 
-            if oneCount/ (totalCount -oneCount) > votingCap
-                one = one + votingCap; 
-            else 
-                one = one + (oneCount/ (totalCount -oneCount)); 
-            end 
-            if twoCount/ (totalCount -twoCount) > votingCap
-                two = two + votingCap; 
-            else 
-                two = two + (twoCount/ (totalCount -twoCount)); 
-            end 
-            if threeCount/ (totalCount -threeCount) > votingCap
-                three = three + votingCap; 
-            else 
-                three = three + (threeCount/ (totalCount -threeCount)); 
-            end 
-            end 
+                totalCount = sum(training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),:));
+                oneCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),1);
+                twoCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),2);
+                threeCount = training_set{i}.intervalCounts(guess{j}.(names{i+numAttributes}),3);
+                % If above voting cap, set to voting cap
+                if oneCount/ (totalCount -oneCount) > votingCap
+                    one = one + votingCap;
+                else
+                    one = one + (oneCount/ (totalCount -oneCount));
+                end
+                if twoCount/ (totalCount -twoCount) > votingCap
+                    two = two + votingCap;
+                else
+                    two = two + (twoCount/ (totalCount -twoCount));
+                end
+                if threeCount/ (totalCount -threeCount) > votingCap
+                    three = three + votingCap;
+                else
+                    three = three + (threeCount/ (totalCount -threeCount));
+                end
+            end
         end
     end
     [~, temp] =  max([one, two, three]);
@@ -40,7 +40,7 @@ for j = 1:size(test,1)
 end
 results.point = test;
 results.classification = classification;
-% Change here for different sizes 
+% Change here for different sizes
 temp = test(:,numAttributes+1) - results.classification';
 results.accuracy = size(find(temp == 0),1);
-end 
+end
